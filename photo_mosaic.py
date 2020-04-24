f60466d193e2e08a4c5f080e0c719dd818849305
emanmacario <emacario1997@hotmail.com>
2020-04-25 00:38:27 +1000

    Saves/loads base image RGB processing output to JSON

--- photo_mosaic.py
+++ photo_mosaic.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 from PIL import Image
+import json
 import numpy as np
-import os
+import os, os.path
 
 """
 1. Read the tile images, which will replace the tiles in the original image.
@@ -80,7 +81,7 @@ def split(image, M=30, N=18):  # DEFAULT M=30, N=18
             total += 1
             yield image.crop((left, upper, right, lower))
 
-    print("Total cropped images creating input: %d", total)
+    print("Total cropped images creating input: %d" % total)
 
 
 def euclidean_distance(rgb1, rgb2):
@@ -99,7 +100,13 @@ def process_base_images():
     that will constitute the photomosaic image
     :return: dictionary of RGB averages
     """
-    base_rgb_avgs = defaultdict(tuple)
+    base_rgb_avgs = load_base_rgb_avgs()
+
+    if base_rgb_avgs is not None:
+        return base_rgb_avgs
+    else:
+        base_rgb_avgs = defaultdict(tuple)
+
     filenames = get_image_filenames()
     total = len(filenames)
     complete = 0
@@ -113,9 +120,47 @@ def process_base_images():
         if complete % 10 == 0:
             print("%4d/%4d complete" % (complete, total))
 
-    # TODO: Write to JSON to avoid long processing times
+    print(base_rgb_avgs)
+
+    # Write dictionary to JSON file to avoid repeated processing
+    save(base_rgb_avgs)
 
     return base_rgb_avgs
+
+
+def save(base_rgb_avgs):
+    """
+    Writes the base image average RGB values to
+    disk as JSON to avoid computing each time
+    :param base_rbg_avgs: dictionary of base image average RGBs
+    :return: None
+    """
+    base_rbg_avgs_copy = {}
+    for f in base_rgb_avgs:
+        base_rbg_avgs_copy[f] = list(base_rgb_avgs[f])
+
+    with open('base_rgb_avgs.json', 'w') as fp:
+        json.dump(base_rbg_avgs_copy, fp,
+                  indent=4,
+                  sort_keys=True)
+
+
+def load_base_rgb_avgs():
+    """
+    Reads in base image average RGB values from JSON file
+    into a dictionary
+    :return: base_avg_rgbs dictionary or None
+    """
+    if os.path.isfile('base_rgb_avgs.json'):
+        # Base images have already been processed,
+        # load JSON into a dictionary
+        with open('base_rgb_avgs.json', 'w') as fp:
+            base_rgb_avgs_json = json.load(fp)
+        base_rgb_avgs = {k: tuple(v) for k, v in base_rgb_avgs_json.items()}
+        return base_rgb_avgs
+
+    # Otherwise, return nothing
+    return None
 
 
 def index_image(image, base_rgb_avgs):
@@ -125,7 +170,7 @@ def index_image(image, base_rgb_avgs):
     :return: filename denoting image with closest average RGB values
     """
     min_distance = np.inf
-    index = ''
+    index = None
     rgb1 = get_avg_rgb(image)
     for im in base_rgb_avgs:
         rgb2 = base_rgb_avgs[im]
@@ -155,6 +200,7 @@ def index_split_images(image, base_rgb_avgs, M=30, N=18):
 
 
 def generate_mosaic(image, index, M=30, N=18):
+    print("Generating mosaic...")
     image = preprocess(image)
     width, height = image.size
 
@@ -162,14 +208,14 @@ def generate_mosaic(image, index, M=30, N=18):
     n = 0
 
     # Create empty final mosaic image
-    mosaic = Image.new('RGB', (width*N, height*M))
+    mosaic = Image.new('RGB', (width*M, height*N))
 
     # Fill in the mosaic with base images
     # by using the index as a reference
-    for i in range(M):
+    for i in range(N):
         # Create a new row
-        row = Image.new('RGB', (width*N, height))
-        for j in range(N):
+        row = Image.new('RGB', (width*M, height))
+        for j in range(M):
             # Append a base image to the row
             im = Image.open('images/' + index[n])
             im = preprocess(im, M, N)
@@ -180,7 +226,9 @@ def generate_mosaic(image, index, M=30, N=18):
         mosaic.paste(row, (0, i*height))
 
     # Save the mosaic image
+    print("Saving mosaic...")
     mosaic.save('mosaics/mosaic.jpg')
+
 
 
 def print_resolutions():
@@ -195,8 +243,7 @@ def print_resolutions():
 
 def main():
     image = Image.open('images/Aatrox_0.jpg')
-    width, height = image.size
-    print(width, height)
+
 
     get_avg_rgb(image)
 
@@ -205,6 +252,7 @@ if __name__ == "__main__":
     # Open and preprocess the input image
     input_file = 'Ahri_7.jpg'
     input_im = Image.open('images/' + input_file)
+
     preprocessed_input_im = preprocess(input_im)
 
     # Process the base images for the mosaic
